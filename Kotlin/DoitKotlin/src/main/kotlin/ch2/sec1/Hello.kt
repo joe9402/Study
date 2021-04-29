8c149787a8a0c759ca2103986e436d26d29d2e44
KS1 <joe9402>
2021-04-29 23:09:20 +0900

    Start Kotlin Study by Doit Kotlin

---
+++ Kotlin/DoitKotlin/src/main/kotlin/ch2/sec1/Hello.kt
@@ -0,0 +1,13 @@
+package ch2.sec1
+
+import kotlin.math.*;
+
+fun main(){
+    val a : String = "HIHI"
+    val num : Int = 20
+    println("print a : $a");
+    println(abs(-12))
+    println(PI)
+
+
+}
