c622bda79d93ad4e1bfa02c5d8f75896349ba1b2
Si Kim <joe9402@naver.com>
2021-10-14 22:41:42 +0900

    Book chapter 3.3 connect with Front server

---
+++ src/main/java/com/example/selfStudy/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.example.selfStudy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        //모든 경로에 대해
+        registry.addMapping("/**")
+                //Origin이 http:localhost:3000 에 대해
+                .allowedOrigins("http://localhost:3000")
+                //아래 6개의 메서드 허용
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+}
