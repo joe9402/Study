3c1b1bf5d0f8e2cbb8151ffea0fc933e1f1cf6f2
Si Kim <joe9402@naver.com>
2021-10-20 00:53:02 +0900

    Book 5.1 ~ 5.3 make lonin page and connect backend

--- react-workspace/todo-react-app/src/service/ApiService.js
+++ react-workspace/todo-react-app/src/service/ApiService.js
@@ -1,14 +1,23 @@
 import { API_BASE_URL } from "../app-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
-    const options = {
-        headers: new Headers({
-            "Content-Type":"application/json",
-        }),
+    let headers = new Headers({
+        "Content-Type":"application/json",
+    });
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    if(accessToken && accessToken !== null) {
+        headers.append("Authorization","Bearer "+accessToken);
+    }
+
+    let options = {
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
-    console.log(options);
+    
+
+    //console.log(options);
     if (request) {
         options.body = JSON.stringify(request);
     }
@@ -19,5 +28,24 @@ export function call(api, method, request) {
             }
             return json;
         })
-    );
+    )
+    .catch((error) => {
+        console.log(error.status);
+        if(error.status === 403) {
+            window.location.href = "/login";
+        }
+        return Promise.reject(error);
+    });
+}
+
+export function signin(userDTO){
+    return call("/auth/signin", "POST", userDTO)
+    .then((response) => {
+        if(response.token) {
+            localStorage.setItem(ACCESS_TOKEN, response.token);
+            window.location.href = "/"
+        }
+        //console.log("response : ", response);
+        //alert("로그인 토큰 : " + response.token);
+    });
 }
