cd4ae0697a4dff27321de9de4a9bed847aa41ffb
Si Kim <joe9402@naver.com>
2021-12-24 23:56:07 +0900

    section2 : 2-5 ~ 2-8

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void join(){
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(memberA);
+
+        Member findMember = memberService.findMember(1L);
+
+        Assertions.assertThat(memberA).isEqualTo(findMember);
+    }
+}
---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        Long memberId = 1L;
+        Member memberA = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Order order = orderService.createOrder(memberId, "ItemA", 100000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+
+
+}
