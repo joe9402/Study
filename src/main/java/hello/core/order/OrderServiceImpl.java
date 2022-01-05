6fd78b642a0a25b0deaaf9ec20f93201bafa8e6b
Si Kim <joe9402@naver.com>
2022-01-05 23:57:22 +0900

    section: 7-1 ~ 7-3

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,13 +5,32 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
+    //@Autowired private MemberRepository memberRepository;// = new MemoryMemberRepository();
+    //@Autowired private DiscountPolicy discountPolicy;// = new FixDiscountPolicy();
     private final MemberRepository memberRepository;// = new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy;// = new FixDiscountPolicy();
 
+    /*@Autowired(required = false)
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("2 discountPolicy : " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("2 memberRepository : " + memberRepository);
+        this.memberRepository = memberRepository;
+    }*/
+
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("1 memberRepository : " + memberRepository);
+        System.out.println("1 discountPolicy : " + discountPolicy);
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
